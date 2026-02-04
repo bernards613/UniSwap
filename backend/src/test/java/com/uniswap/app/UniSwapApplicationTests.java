0f010b79dc7b6ac43cb1e10196f3364ca093d9c5
Bernard Sobierajski <bsobierajski@oakland.edu>
2026-02-04 10:49:57 -0500

    Backend src folders

---
+++ backend/src/test/java/com/uniswap/app/UniSwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniswap.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
