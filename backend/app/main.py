6cd41478a7423069901a1beff9e2592be68d0b06
bernards613 <bsobierajski@oakland.edu>
2026-02-09 13:18:36 -0500

    remove comment

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from app.routers import users, listings, messages, transactions
 app = FastAPI()
 
 app.include_router(users.router)
-#TEST 2 MF
+
 @app.get("/")
 def home():
     return {"message": "Welcome to UniSwap API"}
