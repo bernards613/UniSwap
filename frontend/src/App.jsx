3a832ade75c6c8a328b25d14a0bf2309ec0c0f5f
Ensa <eceesay@oakland.edu>
2026-02-11 11:05:00 -0500

    Created settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Settings } from './Settings'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
@@ -33,3 +34,4 @@ function App() {
 }
 
 export default App
+
---
+++ frontend/src/Settings.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+
+export function Settings() {
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      <p>Settings Page</p>
+      {/*settings options  */}
+    </div>
+  );
+}
